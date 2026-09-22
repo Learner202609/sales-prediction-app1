7289d89c56423eec1e89d1a5284653f66cd4e14c
Learner202609 <hdodamani20@gmail.com>
2026-09-22 18:00:37 -0500

    Fix formatting of predicted sales output

--- frontend/app.py
+++ frontend/app.py
@@ -49,7 +49,7 @@ if st.button("Predict", type='primary'):
     if response.status_code == 200:
         result = response.json()
         predicted_sales = result["Sales"]
-        st.success(f"Predicted Product Store Sales Total: ₹{predicted_sales:.2f}")
+        st.success(f"Predicted Product Store Sales Total: {predicted_sales:.2f}")
     else:
         st.error("Unable to connect to the prediction API.")
 
